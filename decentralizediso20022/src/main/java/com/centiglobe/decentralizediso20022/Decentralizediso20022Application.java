7c8da757ea1383a1d5b74eb28e02464eefcb451a
Cactu5 <cactusdevcode@gmail.com>
2021-03-15 22:12:05 +0100

    Initialize Spring Boot project (#1)
    * Create Spring Boot project
    
    * Fix package name

---
+++ decentralizediso20022/src/main/java/com/centiglobe/decentralizediso20022/Decentralizediso20022Application.java
@@ -0,0 +1,13 @@
+package com.centiglobe.decentralizediso20022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Decentralizediso20022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Decentralizediso20022Application.class, args);
+	}
+
+}
---
+++ decentralizediso20022/src/test/java/com/centiglobe/decentralizediso20022/Decentralizediso20022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centiglobe.decentralizediso20022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decentralizediso20022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
