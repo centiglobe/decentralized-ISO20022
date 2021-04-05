633e3ada561e23893b1389d28e728df69016e72e
Cactu5 <cactusdevcode@gmail.com>
2021-04-05 17:47:02 +0200

    Fix context load test

--- decentralizediso20022/src/test/java/com/centiglobe/decentralizediso20022/Decentralizediso20022ApplicationTests.java
+++ decentralizediso20022/src/test/java/com/centiglobe/decentralizediso20022/Decentralizediso20022ApplicationTests.java
@@ -2,8 +2,10 @@ package com.centiglobe.decentralizediso20022;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Decentralizediso20022ApplicationTests {
 
 	@Test
