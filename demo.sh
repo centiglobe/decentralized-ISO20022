6022bce3fdcc7bd5604747b394ae36f4a0d41898
wsta <william.stacken@gmail.com>
2021-04-15 18:27:35 +0200

    add tag name

--- demo.sh
+++ demo.sh
@@ -2,6 +2,6 @@
 docker network create mynetwork
 docker build -t mockbanksystem mock-bank-system
 docker run -d --network=mynetwork --name=mockbanksystem mockbanksystem
-docker build --network=mynetwork decentralizediso20022
+docker build --network=mynetwork -t decentralizediso20022 decentralizediso20022
 docker run --env PROFILE=internal -d --network=mynetwork --name=decentralizediso20022 decentralizediso20022
 docker run --env PROFILE=external -d --network=mynetwork --name=decentralizediso20022 decentralizediso20022
